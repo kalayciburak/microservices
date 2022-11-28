a2d0621539f17342b59b2b833b30d9450c60f3d8
Burak <torukobyte@gmail.com>
2022-11-28 17:01:30 +0300

    project created

---
+++ inventory-service/src/test/java/com/torukobyte/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.torukobyte.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
