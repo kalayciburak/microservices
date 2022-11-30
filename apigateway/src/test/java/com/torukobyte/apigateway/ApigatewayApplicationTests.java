2f23ba91a5ae16ce1b855c47c8a48295a3780cd3
Burak <kalayciburak1996@gmail.com>
2022-11-30 14:07:35 +0300

    add api-gateway

---
+++ apigateway/src/test/java/com/torukobyte/apigateway/ApigatewayApplicationTests.java
@@ -0,0 +1,12 @@
+package com.torukobyte.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
--- inventory-service/src/main/java/com/torukobyte/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/torukobyte/inventoryservice/InventoryServiceApplication.java
@@ -5,9 +5,11 @@ import com.torukobyte.common.util.mapping.ModelMapperService;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
