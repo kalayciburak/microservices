e26481b7f78443c744190c20ff3a2f1abf1e4004
Burak <kalayciburak1996@gmail.com>
2023-01-05 20:32:55 +0300

    quick fix

--- filter-service/src/main/java/com/kodlamaio/filterservice/api/controllers/FiltersController.java
+++ filter-service/src/main/java/com/kodlamaio/filterservice/api/controllers/FiltersController.java
@@ -4,10 +4,7 @@ import com.kodlamaio.filterservice.business.abstracts.FilterService;
 import com.kodlamaio.filterservice.business.dto.responses.GetAllFiltersResponse;
 import com.kodlamaio.filterservice.business.dto.responses.GetFilterResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
